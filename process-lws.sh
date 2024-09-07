572c0123cee960eaa5ef8c2527b9ba2d8dc4bf78
wensheng li <lws_home@hotmail.com>
2024-09-07 10:01:58 -0400

    version 3.0.6

---
+++ process-lws.sh
@@ -0,0 +1,5 @@
+# Copy latest from master
+cp -r ~/projects/bz-playwright/ex-3/* ex-3/.
+cp -r ~/projects/bz-playwright/index3.js index3.js
+cp -r ~/projects/bz-playwright/logService.js logService.js
+cp -r ~/projects/bz-playwright/package.json package.json
