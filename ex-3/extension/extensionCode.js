a52c59a2214b7a9fbd059f28a12b9666e1805b61
wensheng li <lws_home@hotmail.com>
2024-09-17 12:40:02 -0400

    version 3.0.88

--- ex-3/extension/extensionCode.js
+++ ex-3/extension/extensionCode.js
@@ -15984,6 +15984,7 @@ window.bzComm={
   },
   _handleResult:function(v){
     if(v.return){
+      let cp=bzComm.getCurPageType()1
       let fun=bzComm._callBackMap[v.return].return;
       delete bzComm._callBackMap[v.return];
       if(v.result){
