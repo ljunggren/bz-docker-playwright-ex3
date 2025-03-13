81cd7102cd0ca0287063d4d2e59b6be5cacb206d
ljunggren <mljunggren@gmail.com>
2025-03-13 09:23:58 -0400

    Added dev version

---
+++ dev-build.sh
@@ -0,0 +1,15 @@
+# Cleanup
+rm -rf *.log
+rm -rf *.html
+rm -rf *.png
+rm -rf *.list
+
+set -ex
+
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=styrman
+
+# image name
+IMAGE=boozang-playwright-ex3-dev
+docker build --platform linux/amd64 -t $USERNAME/$IMAGE:latest .
