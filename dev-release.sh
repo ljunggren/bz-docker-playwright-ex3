3ce729c97084cb89bc1d1b23f000bdf6d83da06f
ljunggren <mljunggren@gmail.com>
2025-04-24 10:58:59 -0400

    version 4.0.77

--- dev-release.sh
+++ dev-release.sh
@@ -13,8 +13,32 @@ USERNAME=styrman
 # image name
 IMAGE=boozang-runner-dev
 
+git pull
+# bump version
+docker run --rm -v "$PWD":/app treeder/bump patch
+version=`cat VERSION`
+echo "version: $version"
+
+major=`echo $version | cut -d. -f1`
+minor=`echo $version | cut -d. -f2`
+revision=`echo $version | cut -d. -f3`
+
 # run build
 ./dev-build.sh
 
+# tag it
+git add -A
+git commit -m "version $version"
+git tag -a "$version" -m "version $version"
+git push
+git push --tags
+
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$major
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$major.$minor
+
 # push it
 docker push $USERNAME/$IMAGE:latest
+docker push $USERNAME/$IMAGE:$version
+docker push $USERNAME/$IMAGE:$major
+docker push $USERNAME/$IMAGE:$major.$minor
